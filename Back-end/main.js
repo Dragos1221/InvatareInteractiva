64a64b0c3c30a0b24340e1c547b013b9937f8b7d
Dragos1221 <48069261+Dragos1221@users.noreply.github.com>
2021-04-04 15:31:48 +0300

    create connection to database

--- Back-end/main.js
+++ Back-end/main.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const route = require('./routes')
 const app = express()
+
+const {connection}  = require("./database");
  
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
 app.listen(3000)
+
+
+
+connection.query('select * from test', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+});
